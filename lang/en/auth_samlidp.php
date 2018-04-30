c10f64b915630d34dbdaed037cf880d6f56d524e
otatarintseva <30630279+otatarintseva@users.noreply.github.com>
2018-04-30 13:24:55 +0200

    Add files via upload

---
+++ lang/en/auth_samlidp.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'auth_samlidp', language 'en'.
+ *
+ * @package   auth_samlidp
+ * @copyright 2017 Enovation Solutions {http://enovation.ie}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'SAML Identity Provider';
+$string['auth_samlidpdescription'] = 'This is configuration of an Identity Provider authentication site';
+$string['coderoot_key'] = 'SimpleSAMLphp installation directory';
+$string['coderoot_help'] = 'Absolute path';
+$string['authsource_key'] = 'Auth source';
+$string['authsource_help'] = 'Configured in SimpleSAMLphp in config/authsources.php';
+
