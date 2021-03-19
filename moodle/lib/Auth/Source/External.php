88d086731f7eb67fe1667726b105d5ab3c6bab71
Ivan Matviyuk <ivan.matviyuk@enovation.ie>
2021-03-19 20:05:31 +0000

    Fix deprecation issues. Test with Moodle 3.9 and 3.10

--- moodle/lib/Auth/Source/External.php
+++ moodle/lib/Auth/Source/External.php
@@ -36,21 +36,21 @@
  */
 ################################################################################
 
-class sspmod_moodle_Auth_Source_External extends SimpleSAML_Auth_Source {
+class sspmod_moodle_Auth_Source_External extends SimpleSAML\Auth\Source {
     const STATE_IDENT = 'moodle:External';
-    private $config;
+    private $config = array();
 
     public function __construct($info, $config) {
         assert('is_array($info)');
         assert('is_array($config)');
 
         parent::__construct($info, $config);
-        if (!isset($config{'cookie_name'})) {
-            throw new SimpleSAML_Error_Exception('Misconfiguration in authsources');    # in the moodle part in config/authsources.php there must be 'cookie_name' setting
+        if (!isset($config['cookie_name'])) {
+            throw new SimpleSAML\Error\Exception('Misconfiguration in authsources');    # in the moodle part in config/authsources.php there must be 'cookie_name' setting
         }
-        $ssp_config = SimpleSAML_Configuration::getInstance();
-        $config{'cookie_path'} = $ssp_config->getValue('session.cookie.path');
-        $config{'cookie_salt'} = $ssp_config->getValue('secretsalt');
+        $ssp_config = SimpleSAML\Configuration::getInstance();
+        $config['cookie_path'] = $ssp_config->getValue('session.cookie.path');
+        $config['cookie_salt'] = $ssp_config->getValue('secretsalt');
         $this->config = $config;
     }
 
@@ -63,44 +63,44 @@ class sspmod_moodle_Auth_Source_External extends SimpleSAML_Auth_Source {
         } else {
             # redirect to a login page
             $state['moodle:AuthID'] = $this->authId;
-            $state_id = SimpleSAML_Auth_State::saveState($state, self::STATE_IDENT);
-            $return_to = SimpleSAML_Module::getModuleURL('moodle/resume.php', array('State' => $state_id));
-            $auth_page = $this->config{'login_url'} . '?ReturnTo=' . $return_to;
-            SimpleSAML_Utilities::redirect($auth_page, array('ReturnTo' => $return_to));
+            $state_id = SimpleSAML\Auth\State::saveState($state, self::STATE_IDENT);
+            $return_to = SimpleSAML\Module::getModuleURL('moodle/resume.php', array('State' => $state_id));
+            $auth_page = $this->config['login_url'] . '?ReturnTo=' . $return_to;
+            SimpleSAML\Utilities::redirect($auth_page, array('ReturnTo' => $return_to));
         }
     }
 
     public static function resume() {
         if (!isset($_REQUEST['State'])) {
-            throw new SimpleSAML_Error_BadRequest('Missing "State" parameter.');
+            throw new SimpleSAML\Error\BadRequest('Missing "State" parameter.');
         }
         $state_id = (string)$_REQUEST['State'];
 
-        $state = SimpleSAML_Auth_State::loadState($state_id, self::STATE_IDENT);
-        $source = SimpleSAML_Auth_Source::getById($state['moodle:AuthID']);
+        $state = SimpleSAML\Auth\State::loadState($state_id, self::STATE_IDENT);
+        $source = SimpleSAML\Auth\Source::getById($state['moodle:AuthID']);
         if ($source === NULL) {
-            throw new SimpleSAML_Error_Exception('Could not find authentication source with id ' . $state[self::AUTHID]);
+            throw new SimpleSAML\Error\Exception('Could not find authentication source with id ' . $state[self::AUTHID]);
         }
 
         if (! ($source instanceof self)) {
-            throw new SimpleSAML_Error_Exception('Authentication source type changed.');
+            throw new SimpleSAML\Error\Exception('Authentication source type changed.');
         }
 
         $user = $source->getUser($state);
         if ($user === NULL) {
-            throw new SimpleSAML_Error_Exception('User not authenticated after login page.');
+            throw new SimpleSAML\Error\Exception('User not authenticated after login page.');
         }
         $state['Attributes'] = $user;
 
-        SimpleSAML_Auth_Source::completeAuth($state);
+        SimpleSAML\Auth\Source::completeAuth($state);
         exit(); # should never reach here
     }
 
     private function getUser() {
         $uid = 0;
-        if (isset($_COOKIE{$this->config{'cookie_name'}}) && $_COOKIE{$this->config{'cookie_name'}}) {
-            $str_cookie = $_COOKIE{$this->config{'cookie_name'}};
-            # cookie created by: "setcookie($cookieName{'cookie_name'}, hash_hmac('sha1', $salt.$account->uid, $salt).':'.$uid, 0, $sspConfig->getValue('session.cookie.path'));"
+        if (isset($_COOKIE[$this->config['cookie_name']]) && $_COOKIE[$this->config['cookie_name']]) {
+            $str_cookie = $_COOKIE[$this->config['cookie_name']];
+            # cookie created by: "setcookie($cookieName['cookie_name'], hash_hmac('sha1', $salt.$account->uid, $salt).':'.$uid, 0, $sspConfig->getValue('session.cookie.path'));"
             # in auth/samlidp/auth.php in Moodle
             $arr_cookie = explode(':', $str_cookie);
 
@@ -108,17 +108,17 @@ class sspmod_moodle_Auth_Source_External extends SimpleSAML_Auth_Source {
                 && (isset($arr_cookie[1]) && $arr_cookie[1])
             ) {
                 # make sure no one manipulated the hash or the uid in the cookie before we trust the uid
-                if (hash_hmac('sha1', $this->config{'cookie_salt'}.$arr_cookie[1], $this->config{'cookie_salt'}) == $arr_cookie[0]) {
+                if (hash_hmac('sha1', $this->config['cookie_salt'].$arr_cookie[1], $this->config['cookie_salt']) == $arr_cookie[0]) {
                     $uid = (int)$arr_cookie[1];
                 } else {
-                    throw new SimpleSAML_Error_Exception('Cookie hash invalid.');
+                    throw new SimpleSAML\Error\Exception('Cookie hash invalid.');
                 }
             }
         }
 
         # our cookie must be removed here
-        if (isset($_COOKIE{$this->config{'cookie_name'}})) {
-            setcookie($this->config{'cookie_name'}, "", time() - 3600, $this->config{'cookie_path'});
+        if (isset($_COOKIE[$this->config['cookie_name']])) {
+            setcookie($this->config['cookie_name'], "", time() - 3600, $this->config['cookie_path']);
         }
 
         if ($uid) {
@@ -126,18 +126,18 @@ class sspmod_moodle_Auth_Source_External extends SimpleSAML_Auth_Source {
             global $CFG, $DB;
             define('CLI_SCRIPT', true);
             define('WEB_CRON_EMULATED_CLI', 'defined');
-            $configphp = $this->config{'moodle_coderoot'}."/config.php";
-            $userlib = $this->config{'moodle_coderoot'}."/user/profile/lib.php";
+            $configphp = $this->config['moodle_coderoot']."/config.php";
+            $userlib = $this->config['moodle_coderoot']."/user/profile/lib.php";
             if (file_exists($configphp)) {
                 require_once($configphp);
             } else {
-                throw new SimpleSAML_Error_Exception('Moodle app instantiation failure: cannot require()' . $configphp);
+                throw new SimpleSAML\Error\Exception('Moodle app instantiation failure: cannot require()' . $configphp);
             }
 
             if (file_exists($userlib)) {
                 require_once($userlib);
             } else {
-                throw new SimpleSAML_Error_Exception('Moodle app instantiation failure: cannot require()' . $userlib);
+                throw new SimpleSAML\Error\Exception('Moodle app instantiation failure: cannot require()' . $userlib);
             }
 
             # query for a user
@@ -152,10 +152,10 @@ class sspmod_moodle_Auth_Source_External extends SimpleSAML_Auth_Source {
                 unset($user->secret);
                 $user->uid = $user->id;     # i dont believe this is strictly necessary. just nice-to-have
                 foreach ((array)$user as $param => $value) {
-                    $userattr{$param} = array($value);
+                    $userattr[$param] = array($value);
                 }
                 foreach ($profile_fields as $param => $value) {
-                    $userattr{'profile_field_'.$param} = array($value);
+                    $userattr['profile_field_'.$param] = array($value);
                 }
             }
             return $userattr;
@@ -170,17 +170,17 @@ class sspmod_moodle_Auth_Source_External extends SimpleSAML_Auth_Source {
             session_start();
         }
 
-        if (isset($_COOKIE{$this->config{'cookie_name'}})) {
-            setcookie($this->config{'cookie_name'}, "", time() - 3600, $this->config{'cookie_path'});
+        if (isset($_COOKIE[$this->config['cookie_name']])) {
+            setcookie($this->config['cookie_name'], "", time() - 3600, $this->config['cookie_path']);
         }
         session_destroy();
 
-        $logout_url = $this->config{'logout_url'};
+        $logout_url = $this->config['logout_url'];
         if (!empty($state['ReturnTo'])) {
             $logout_url .= '?ReturnTo=' . $state['ReturnTo'];
         }
 
-        SimpleSAML_Utilities::redirect($logout_url);
+        SimpleSAML\Utilities::redirect($logout_url);
         die();
     }
 }
